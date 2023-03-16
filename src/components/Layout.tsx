9fba8cf3c6bb9ee5711e0279aaab7bf090f50a60
Marina Kovel <waveee@gmail.com>
2023-03-16 20:13:51 +0300

    feat: add styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import './layout.scss';
 import { NavLink, Outlet } from 'react-router-dom';
 
 class Layout extends React.Component {
   render() {
     return (
       <>
-        <header>
-          <NavLink to="/">Main</NavLink>
-          <NavLink to="/about">About us</NavLink>
+        <header className="header">
+          <NavLink to="/" className="navlink">
+            Main
+          </NavLink>
+          <NavLink to="/about" className="navlink">
+            About us
+          </NavLink>
         </header>
         <Outlet />
-        <footer>2023</footer>
+        <footer className="footer">2023</footer>
       </>
     );
   }
