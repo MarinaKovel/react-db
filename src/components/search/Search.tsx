b14cd70b30e86be45a858a88dc76e678a5077bf3
Marina Kovel <waveee@gmail.com>
2023-04-03 00:05:20 +0300

    fix: fix search saving to local storage

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Search() {
   const [search, setSearch] = useState(localStorage.getItem('search') || '');
+  const inputRef = useRef(search);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
   }
+
+  useEffect(() => {
+    inputRef.current = search;
+  }, [search]);
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('search', inputRef.current);
+    };
+  }, []);
 
   return (
     <form>
       <input
         type="search"
         value={search}
+        ref={() => search}
         placeholder="Search"
         onChange={handleChange}
-        onBlur={() => localStorage.setItem('search', search)}
       />
     </form>
   );
